6d25b6c343e1950b4b49acbcf1c3f3ba636b6a32
Juan Lucha <juanlucha@gmail.com>
2021-09-12 11:24:28 +0200

    Make the library hybrid (esm, cjs, umd)

---
+++ rollup.config.js
@@ -0,0 +1,42 @@
+import { terser } from 'rollup-plugin-terser'
+
+export default {
+  input: 'src/index.js',
+  output: [
+    {
+      file: 'dist/esm.js',
+      format: 'esm',
+      sourcemap: true,
+    },
+    {
+      file: 'dist/esm.min.js',
+      format: 'esm',
+      plugins: [terser()],
+      sourcemap: true,
+    },
+    {
+      file: 'dist/cjs.cjs',
+      format: 'cjs',
+      sourcemap: true,
+    },
+    {
+      file: 'dist/cjs.min.cjs',
+      format: 'cjs',
+      plugins: [terser()],
+      sourcemap: true,
+    },
+    {
+      file: 'dist/umd.js',
+      format: 'umd',
+      name: 'solo-rpg-lib',
+      sourcemap: true,
+    },
+    {
+      file: 'dist/umd.min.js',
+      format: 'umd',
+      name: 'solo-rpg-lib',
+      plugins: [terser()],
+      sourcemap: true,
+    },
+  ],
+}
